e9a2018291a7c9b0ff69ac8bce2a039fc51eef9b
SUPER123 <sunl25334@gmail.com>
2026-07-21 16:16:34 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/_geehy_reclose/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'geehy_reclose' 
+ * Target:  'geehy_reclose' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "apm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
